939d74675d8b2ba07eaab3c2cc6394f1b279a7be
shota <s8152.45@gmail.com>
2025-06-04 19:25:39 +0900

    first commit

---
+++ pg_practice.sql
@@ -0,0 +1,5 @@
+-- 問題1. postgresqlでpracticeデータベースを生成するSQL文を記述してください。
+
+-- 問題2. postgresqlでpracticeデータベースのusersテーブルを生成するSQL文を記述してください。
+
+-- 問題3. postgresqlでpracticeデータベースのjobsテーブルを生成するSQL文を記述してください。
